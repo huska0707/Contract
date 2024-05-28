50d91ce3e68e914922e4a88114286cedd5a271e0
huska0707 <hussaink001haled@gmail.com>
2024-05-28 11:37:52 -0500

    Charm attribute of the character

--- contracts/breakInGame.sol
+++ contracts/breakInGame.sol
@@ -95,5 +95,6 @@ contract BreakInGame is VRFConsumerBase, Ownable, KeeperCompatibleInterface {
         uint256 agility;
         uint256 strength;
         uint256 sneak;
+        uint256 charm;
     }
 }
