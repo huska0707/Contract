ac743741d85336df16f0ed1afdf9608dba5ddc80
huska0707 <hussaink001haled@gmail.com>
2024-05-28 16:56:15 -0500

    Name of the character

--- contracts/breakInNFTMinter.sol
+++ contracts/breakInNFTMinter.sol
@@ -79,5 +79,5 @@ contract NFTMint is
 
     NFTCharacter[] public characters;
 
-    function addCharacterOne(uint256 health, string memory imageURI,) public {}
+    function addCharacterOne(uint256 health, string memory imageURI,string memory name,) public {}
 }
