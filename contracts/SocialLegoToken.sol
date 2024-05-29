f2830b7ab72f9bb2f3baf63d14f41145f913af83
huska0707 <hussaink001haled@gmail.com>
2024-05-29 08:44:13 -0500

    SocialLegoToken contract

---
+++ contracts/SocialLegoToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.3.2 (token/ERC20/ERC20.sol)
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+contract SocialLegoToken is Context, IERC20, IERC20Metadata {}
