f39ebae67d0d652d1f26148be0379d888e8ecef9
huska0707 <hussaink001haled@gmail.com>
2024-05-29 09:08:16 -0500

    function mint to the zero address

--- contracts/SocialLegoToken.sol
+++ contracts/SocialLegoToken.sol
@@ -134,4 +134,16 @@ contract SocialLegoToken is Context, IERC20, IERC20Metadata {
 
         _afterTokenTransfer(sender, recipient, amount);
     }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
 }
